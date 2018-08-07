bf9533d4768ee939174258dcc1c6d15c11c61717
jenniferzrd <jennifer.zarid@gmail.com>
2018-08-07 19:15:19 +0200

    [ui] ajout donnees mock cards gift

--- front-chooseTheGift/src/app/app.module.ts
+++ front-chooseTheGift/src/app/app.module.ts
@@ -9,19 +9,29 @@ import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { UiComponent } from './ui/ui.component';
+import { IdeasComponent } from './ideas/ideas.component';
+import { CreateIdeaComponent } from './create-idea/create-idea.component';
+import { HomeComponent } from './home/home.component';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    CreateUserComponent
+    CreateUserComponent,
+    UiComponent,
+    IdeasComponent,
+    CreateIdeaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    // NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-chooseTheGift/src/app/app.routes.ts
+++ front-chooseTheGift/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
-    { path: 'home', component: AppComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'index', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'users', component: UserComponent },
     { path: 'createusers', component: CreateUserComponent },
 ];
